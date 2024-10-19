669634c4ce7bd11f8a54c375195053ba61b29da9
mohammad arif <rifrocket@mohammads-MacBook-Pro.local>
2024-10-19 23:15:43 +0530

    update functionality

--- install-lamp.sh
+++ install-lamp.sh
@@ -5,21 +5,37 @@ pass="testT8080"
 php_versions=("8.2")
 install_supervisor=false
 web_server="apache"
-
-# Show help message
+install_mysql=false
+install_php=false
+install_phpmyadmin=false
+install_phpmyadmin_pass="testT8080"
 force_reinstall=false
 remove_lamp=false
+install_lamp=false
+install_lemp=false
 
+# Show help message
 show_help() {
-  echo "Usage: install-lamp.sh [options]"
-  echo "Options:"
-  echo "  -w, --web-server       Set web server (apache or nginx) (default: apache)"
-  echo "  -p, --password         Set MySQL root password (default: testT8080)"
-  echo "  -v, --php-versions     Install PHP versions (default: 8.2)"
-  echo "  --supervisor           Install Supervisor for process management (default: false)"
-  echo "  -h, --help             Show this help message"
+  printf "Usage: install-lamp.sh [options]\n"
+  printf "\nOptions:\n\n"
+  printf "  --lamp                    Install LAMP stack (default: false, use flag to enable)\n"
+  printf "  --lemp                    Install LEMP stack (default: false, use flag to enable)\n"
+  printf "\nCustomization Options:\n\n"
+  printf "  --web-server              Set web server (apache or nginx) (default: apache)\n"
+  printf "  --mysql                   Install MySQL (default: false, use flag to enable)\n"
+  printf "  --mysql-password          Set MySQL root password (default: testT8080)\n"
+  printf "  --php                     Install PHP (default: false, default version: 8.2, specify version with --php=<version>)\n"
+  printf "  --phpmyadmin              Install phpMyAdmin (default: false, use flag to enable)\n"
+  printf "  --phpmyadmin-password     Set phpMyAdmin password (default: testT8080)\n"
+  printf "  --supervisor              Install Supervisor (default: false, use flag to enable)\n"
+  printf "  --remove                  Remove existing LAMP or LEMP stack (default: false, use flag to enable)\n"
+  printf "  -h, --help                Show this help message\n"
+  printf "\nExamples:\n\n"
+  printf "  ./install-lamp.sh --lamp\n"
+  printf "  ./install-lamp.sh --lemp\n"
+  printf "  ./install-lamp.sh --lamp --mysql-password=mysecurepassword\n"
+  printf "  ./install-lamp.sh --web-server=nginx --php=8.2 --mysql --mysql-password=mysqlpass --phpmyadmin --phpmyadmin-password=myadminpass\n\n"
 }
-
 
 # Get server IP
 get_server_ip() {
@@ -46,10 +62,11 @@ run_update() {
   echo "+--------------------------------------+"
   sudo apt-get update -qq
   echo "+--------------------------------------+"
-  echo "|     system packages Updated          |"
+  echo "|     System packages updated          |"
   echo "+--------------------------------------+"
 }
 
+# Install Apache
 install_apache() {
   echo "+--------------------------------------+"
   echo "|     Installing Apache                |"
@@ -60,53 +77,47 @@ install_apache() {
   sudo systemctl enable apache2
   sudo apache2ctl configtest
 
+  if [ $? -ne 0 ]; then
+    echo "ERROR: Apache installation failed. Check the Apache configuration."
+    exit 1
+  fi
+
   sudo tee /var/www/html/index.html > /dev/null <<END
-  <!DOCTYPE html>
-  <html>
-  <head>
-  <title>Welcome to Apache!</title>
-  </head>
-  <body>
-  <h1>Apache is installed!</h1>
-  <?php
-    phpinfo();
-  ?>
-  </body>
-  </html>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Apache!</title>
+</head>
+<body>
+<h1>Apache is installed!</h1>
+<?php phpinfo(); ?>
+</body>
+</html>
 END
 
-
-#check if apache2ctl configtest is successful
-if [ $? -ne 0 ]; then
-  echo "ERROR: Apache installation failed. Check the Apache configuration."
-  exit 1
-fi
-
-echo "+--------------------------------------+"
-echo "|     Apache Installed Successfully    |"
-echo "+--------------------------------------+"
-
+  echo "+--------------------------------------+"
+  echo "|     Apache Installed Successfully    |"
+  echo "+--------------------------------------+"
 }
 
-# Function to install and configure Nginx
+# Install Nginx
 install_nginx() {
-    echo "+--------------------------------------+"
-    echo "|     Installing Nginx                 |"
-    echo "+--------------------------------------+"
+  echo "+--------------------------------------+"
+  echo "|     Installing Nginx                |"
+  echo "+--------------------------------------+"
 
-    # Install Nginx
-    sudo apt-get update
-    sudo apt-get install -y nginx
-    
-    # Enable Nginx to start at boot and start it now
-    sudo systemctl enable nginx
-    sudo systemctl start nginx || { echo "Failed to start Nginx."; exit 1; }
+  sudo apt-get install -y nginx
 
-    # Install PHP and PHP-FPM
-    sudo apt-get install -y php8.2-fpm php8.2-mbstring php8.2-xml php8.2-mysql
+  sudo systemctl enable nginx
+  sudo systemctl start nginx || { echo "Failed to start Nginx."; exit 1; }
 
-    # Configure Nginx to use PHP-FPM
-    sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+  # Install PHP-FPM
+  for version in "${php_versions[@]}"; do
+    sudo apt-get install -y php${version}-fpm
+  done
+
+  # Configure Nginx to use PHP-FPM
+  sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
 server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -122,7 +133,7 @@ server {
 
     location ~ \.php\$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php8.2-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php${php_versions[0]}-fpm.sock;
     }
 
     location ~ /\.ht {
@@ -131,50 +142,12 @@ server {
 }
 EOF
 
-    # Test Nginx configuration and reload
-    sudo nginx -t && sudo systemctl reload nginx
-
-    # Ensure PHP-FPM is running
-    sudo systemctl enable php8.2-fpm
-    sudo systemctl start php8.2-fpm
-
-    # Create a custom Nginx welcome page
-    sudo tee /var/www/html/index.html > /dev/null <<EOF
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to Your Nginx Server!</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            background-color: #f4f4f4;
-        }
-        h1 {
-            color: #333;
-        }
-        p {
-            color: #666;
-        }
-    </style>
-</head>
-<body>
-    <h1>Welcome to Your Nginx Server!</h1>
-    <p>The Nginx web server is installed and running on your server.</p>
-    <p>PHP version: <?php echo phpversion(); ?></p>
-</body>
-</html>
-EOF
-
-    echo "Custom Nginx welcome page created successfully."
-    echo "Nginx installed and configured successfully."
+  sudo nginx -t && sudo systemctl reload nginx
+  echo "Custom Nginx welcome page created successfully."
+  echo "+--------------------------------------+"
+  echo "|  Nginx installed and configured      |"
+  echo "+--------------------------------------+"
 }
-
-
-
 
 # Install MySQL
 install_mysql() {
@@ -198,73 +171,65 @@ install_mysql() {
   echo "+--------------------------------------+"
 }
 
-
-# Function to detect OS and add the appropriate PHP repository
+# Add PHP repository based on OS
 add_php_repository() {
   echo "+--------------------------------------+"
   echo "|     Adding PHP Repository            |"
   echo "+--------------------------------------+"
 
-  # Get the OS details
   os_name=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
   os_version=$(lsb_release -cs)
 
   if [[ "$os_name" == "ubuntu" ]]; then
-    # Add PHP PPA for Ubuntu
     sudo apt install software-properties-common -y
     sudo add-apt-repository ppa:ondrej/php -y
-    sudo apt-get update
   elif [[ "$os_name" == "debian" ]]; then
-    # Add PHP repository for Debian
     sudo apt install -y apt-transport-https lsb-release ca-certificates curl
     curl -fsSL https://packages.sury.org/php/README.txt | sudo bash -x
-    sudo apt-get update
   else
     echo "Unsupported OS: $os_name"
     exit 1
   fi
 
+  sudo apt-get update
   echo "+--------------------------------------+"
   echo "|    PHP Repository Added Successfully |"
   echo "+--------------------------------------+"
 }
 
-
-# Install PHP and the required extensions
+# Install PHP and required extensions
 install_php() {
   local php_version=$1
   echo "+--------------------------------------+"
-  echo "|     Installing PHP $php_version"
+  echo "|     Installing PHP $php_version      |"
   echo "+--------------------------------------+"
-  sudo apt-get install -y php$php_version libapache2-mod-php$php_version php$php_version-mysql php$php_version-curl php$php_version-gettext >  /dev/null 2>&1
+
+  sudo apt-get install -y php$php_version libapache2-mod-php$php_version php$php_version-mysql php$php_version-curl php$php_version-gettext > /dev/null 2>&1
   sudo apt-get install -y php$php_version-mbstring php$php_version-zip php$php_version-gd php$php_version-common php$php_version-xml php$php_version-bcmath php$php_version-fpm > /dev/null 2>&1
+
   if [ $? -ne 0 ]; then
     echo "ERROR: Failed to install PHP $php_version."
     exit 1
   fi
 
-  # Set PHP $php_version as the default PHP version
+  # Set PHP version as default
   sudo update-alternatives --set php /usr/bin/php$php_version
   sudo update-alternatives --set phpize /usr/bin/phpize$php_version
   sudo update-alternatives --set php-config /usr/bin/php-config$php_version
 
-
-  sudo a2enmod php$php_version
-  sudo systemctl restart apache2
-
-
-
-  if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to enable PHP $php_version module."
-    exit 1
+  if [ "$web_server" == "apache" ]; then
+    sudo a2enmod php$php_version
+    sudo systemctl restart apache2
+  elif [ "$web_server" == "nginx" ]; then
+    sudo systemctl restart php$php_version-fpm
   fi
 
   echo "+--------------------------------------+"
-  echo "|    PHP $php_version Installed"
+  echo "|    PHP $php_version Installed       |"
   echo "+--------------------------------------+"
 }
 
-# Install PhpMyAdmin
+# Install phpMyAdmin
 install_phpmyadmin() {
   local pass=$1
   echo "+--------------------------------------+"
@@ -278,10 +243,9 @@ install_phpmyadmin() {
   echo "phpmyadmin phpmyadmin/mysql/app-pass password $pass" | sudo debconf-set-selections
   echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
 
-  # Install phpMyAdmin and required PHP extensions
+  # Install phpMyAdmin
   sudo apt-get install -y phpmyadmin
 
-  # Check if phpMyAdmin was successfully installed
   if [ $? -ne 0 ]; then
     echo "ERROR: Failed to install PhpMyAdmin."
     exit 1
@@ -291,19 +255,14 @@ install_phpmyadmin() {
   sudo phpenmod mbstring
   sudo phpenmod gettext
 
-  # Check if symbolic link exists and remove it if necessary
+  # Remove existing symlink if exists
   if [ -L /var/www/html/phpmyadmin ] || [ -d /var/www/html/phpmyadmin ]; then
-    echo "Existing phpMyAdmin directory or symlink found. Removing it..."
+    echo "Existing phpMyAdmin directory found. Removing it..."
     sudo rm -rf /var/www/html/phpmyadmin
   fi
 
   # Create a new symbolic link to phpMyAdmin
   sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-
-  if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to create symbolic link for PhpMyAdmin."
-    exit 1
-  fi
 
   # Restart Apache to apply changes
   sudo systemctl restart apache2
@@ -312,8 +271,6 @@ install_phpmyadmin() {
   echo "|  PhpMyAdmin Installed Successfully   |"
   echo "+--------------------------------------+"
 }
-
-
 
 # Install Supervisor
 install_supervisor() {
@@ -340,101 +297,54 @@ remove_existing_installation() {
   echo "|  Removing Existing LAMP Installation |"
   echo "+--------------------------------------+"
 
-  # Stop Apache and MySQL services first to avoid issues during removal
+  # Stop services to avoid issues during removal
   sudo systemctl stop apache2 mysql
 
-  # Purge Apache, MySQL, PHP, and phpMyAdmin
-  sudo apt-get purge -y apache2 apache2-utils apache2-bin apache2.2-common
-  sudo apt-get purge -y mysql-server mysql-client mysql-common
-  sudo apt-get purge -y php* phpmyadmin
-
-  # Autoremove unnecessary dependencies and clean up apt cache
+  # Purge installed components
+  sudo apt-get purge -y apache2 mysql-server php* phpmyadmin
   sudo apt-get autoremove -y
   sudo apt-get autoclean -y
 
-  # Remove leftover configuration files and data directories
-  sudo rm -rf /etc/apache2
-  sudo rm -rf /etc/mysql
-  sudo rm -rf /etc/php
-  sudo rm -rf /etc/phpmyadmin
-
-  # Optionally remove MySQL databases (CAUTION: This will delete all your MySQL data)
-  sudo rm -rf /var/lib/mysql
-  sudo rm -rf /var/www/html
-  
-  # Clean up other residual files
-  sudo rm -rf /var/log/apache2
-  sudo rm -rf /var/log/mysql
-  sudo rm -rf /var/cache/apache2
-  sudo rm -rf /usr/share/phpmyadmin
+  # Remove configuration files
+  sudo rm -rf /etc/apache2 /etc/mysql /etc/php /usr/share/phpmyadmin
 
   echo "+--------------------------------------+"
   echo "|   Existing Installation Removed      |"
   echo "+--------------------------------------+"
 }
 
-
-# Function to fix deprecated http_build_query usage in PhpMyAdmin
-fix_phpmyadmin_deprecation() {
-    echo "+--------------------------------------+"
-    echo "|     Fixing PhpMyAdmin Deprecation    |"
-    echo "+--------------------------------------+"
-
-    # Path to the PhpMyAdmin Url.php file
-    PHPMYADMIN_URL_PHP="/usr/share/phpmyadmin/libraries/classes/Url.php"
-
-    # Check if the file exists
-    if [ -f "$PHPMYADMIN_URL_PHP" ]; then
-        # Use sed to find and replace the deprecated code
-        sudo sed -i 's/http_build_query(\$queryParams, null, /http_build_query(\$queryParams, "", /' "$PHPMYADMIN_URL_PHP"
-
-        if [ $? -eq 0 ]; then
-            echo "PhpMyAdmin deprecation issue fixed successfully."
-        else
-            echo "ERROR: Failed to fix PhpMyAdmin deprecation issue."
-            exit 1
-        fi
-    else
-        echo "ERROR: PhpMyAdmin Url.php file not found."
-        exit 1
-    fi
-}
-
-
 # Parse arguments
 while [ "$1" != "" ]; do
   case "$1" in
-    -w | --web-server ) web_server=$2; shift 2;;
-    -p | --password ) pass=$2; shift 2;;
-    -v | --php-versions ) IFS=',' read -r -a php_versions <<< "$2"; shift 2;;
+    --web-server ) web_server=$2; shift 2;;
+    --mysql ) install_mysql=true; shift;;
+    --mysql-password ) pass=$2; shift 2;;
+    --php ) install_php=true; php_versions=($2); shift 2;;  # Allow setting PHP versions
+    --phpmyadmin ) install_phpmyadmin=true; shift;;
+    --phpmyadmin-password ) install_phpmyadmin_pass=$2; shift 2;;
     --supervisor ) install_supervisor=true; shift;;
-    -h | --help ) show_help; exit;;
-    --force-reinstall ) force_reinstall=true; shift;;
+    --lamp ) install_lamp=true; shift;;
+    --lemp ) install_lemp=true; shift;;
     --remove ) remove_lamp=true; shift;;
+    -h | --help ) show_help; exit;;
     * ) echo "Unknown option: $1"; show_help; exit 1;;
   esac
 done
 
 # Remove LAMP stack if --remove flag is passed
 if [ "$remove_lamp" = true ]; then
-  echo "Removing LAMP stack..."
+  echo "Removing LAMP or LEMP stack..."
   remove_existing_installation
   exit 0
 fi
 
-# start the installation process
-
-
-# Add PHP repository to ensure the latest PHP versions are available
-add_php_repository
-
-# Check if LAMP stack is already installed
-if is_lamp_installed; then
+# Check if LAMP or LEMP stack is already installed
+if is_lamp_installed || is_lemp_installed; then
   if [ "$force_reinstall" = true ]; then
     echo "Forcing reinstallation..."
     remove_existing_installation
   else
-    echo "LAMP stack seems to be already installed. Use --force-reinstall flag to reinstall or --remove to uninstall."
+    echo "LAMP or LEMP stack seems to be already installed. Use --force-reinstall flag to reinstall or --remove to uninstall."
     exit 1
   fi
 fi
@@ -442,22 +352,36 @@ fi
 # Run the installation steps
 run_update
 
-# Install Server
-if [ "$web_server" == "apache" ]; then
-  install_apache
-elif [ "$web_server" == "nginx" ]; then
-  install_nginx
-else
-  echo "Unsupported web server: $web_server"
-  exit 1
+# Install Web Server
+if [ "$install_lamp" = true ] || [ "$install_lemp" = true ]; then
+  if [[ "$web_server" == "apache" ]]; then
+    install_apache
+  elif [[ "$web_server" == "nginx" ]]; then
+    install_nginx
+  else
+    echo "Unsupported web server: $web_server. Use 'apache' or 'nginx'."
+    exit 1
+  fi
 fi
 
-install_mysql "$pass"
-install_php "$php_versions"
-install_phpmyadmin "$pass"
-fix_phpmyadmin_deprecation
+# Install MySQL
+if [[ "$install_mysql" = true ]] || [[ "$install_lamp" = true ]] || [[ "$install_lemp" = true ]]; then
+  install_mysql "$pass"
+fi
 
-# check if supervisor is to be installed
+# Install PHP
+if [[ "$install_php" = true ]] || [[ "$install_lamp" = true ]] || [[ "$install_lemp" = true ]]; then
+  for version in "${php_versions[@]}"; do
+    install_php "$version"
+  done
+fi
+
+# Install phpMyAdmin
+if [[ "$install_phpmyadmin" = true ]] || [[ "$install_lamp" = true ]] || [[ "$install_lemp" = true ]]; then
+  install_phpmyadmin "$install_phpmyadmin_pass"
+fi
+
+# Check if Supervisor is to be installed
 if [ "$install_supervisor" = true ]; then
   install_supervisor
 fi
@@ -472,10 +396,13 @@ fi
 
 # Display completion message
 ip=$(get_server_ip)
+if(ip == "")
+then
+  ip="localhost"
+fi
 echo "+-------------------------------------------+"
-echo "|    Finish Auto Install and Setup LAMP"
-echo "|"
-echo "| Web Site: http://$ip/"
-echo "| PhpMyAdmin: http://$ip/phpmyadmin"
-echo "| User: root || Pass: $pass"
+echo "|    Finish Auto Install and Setup LAMP    |"
+echo "| Web Site: http://$ip/                    |"
+echo "| PhpMyAdmin: http://$ip/phpmyadmin        |"
+echo "| User: root || Pass: $pass                |"
 echo "+-------------------------------------------+"
